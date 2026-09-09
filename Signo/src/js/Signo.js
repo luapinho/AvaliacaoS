70a0f7d0120fe0b2511a370737442682392c8417
unknown <2amod@e041edu.local>
2026-09-09 15:10:23 -0300

    Feat: Adicionando vetor e verificações

--- Signo/src/js/Signo.js
+++ Signo/src/js/Signo.js
@@ -1,55 +1,143 @@
+var dia_semana = new Array();
+dia_semana[0] = "um domingo";
+dia_semana[1] = "uma segunda-feira";
+dia_semana[2] = "uma terça-feira";
+dia_semana[3] = "uma quarta-feira";
+dia_semana[4] = "uma quinta-feira";
+dia_semana[5] = "uma sexta-feira";
+dia_semana[6] = "um sábado";
+
+var nome_mes = new Array(); //vetor dinamico(matriz, arranjo): Array(metodo construtor/ função(): sem função usa [metodos contados])
+nome_mes[0] = "Janeiro";
+nome_mes[1] = "Fevereiro"; //indice(dentro conchetes: onde o indice que o usuario escrever, séra colocado o valor)
+nome_mes[2] = "Março";
+nome_mes[3] = "Abril";
+nome_mes[4] = "Maio";
+nome_mes[5] = "Junho";
+nome_mes[6] = "Julho";
+nome_mes[7] = "Agosto";
+nome_mes[8] = "Setembro";
+nome_mes[9] = "Outubro";
+nome_mes[10] = "Novembro";
+nome_mes[11] = "Dezembro";
+
 function Calcular() {
 
-    var d_ani = document.getElementById("dia").value;
-    var m_ani = document.getElementById("mes").value;
-    var ano = document.getElementById("ano").value;
+    var d_ani = document.getElementById("dia").value; // var(quando o valor pode ser alterado)
+    var m_ani = document.getElementById("mes").value; // let(valor já adicionado)
+    var a_ani = document.getElementById("ano").value; // const(quando o valor não muda)
 
-    if (d_ani <= 0 || isNaN(d_ani) || d_ani > 31){
+    if (d_ani <= 0 || isNaN(d_ani) || d_ani > 31) {
         alert("Dia inválido, dia está vazio ou não é númerico ou o dia está ultrapassando o 31.");
         return;
     }
-    if (m_ani < 1 || isNaN(m_ani) || m_ani > 12){
+    else {
+        d_ani = parseInt(d_ani);
+    }
+
+    if (m_ani < 1 || isNaN(m_ani) || m_ani > 12) {
         alert("Mês inválido, mês está vazio ou não é númerico ou o mes passou de 12.");
         return;
     }
+    else {
+        m_ani = parseInt(m_ani);
+    }
 
-    if (ano <= 0 || isNaN(ano) || ano.length != 4){
+    if (a_ani <= 0 || isNaN(a_ani) || a_ani.length != 4) {
         alert("Ano inválido, ano está vazio ou não é númerico ou não pode maior que 4 dígitos.");
         return;
     }
+    else {
+        a_ani = parseInt(a_ani);
 
-    if (((d_ani >= 20) && (m_ani == 1)) || ((d_ani <= 18) && (m_ani == 2)))
-        document.images["signo"].src = "src/assets/images/aquario.png"
+        //depois de tudo verificado,instanciando o objeto Date() data do sistema		
+        var hoje = new Date();
 
-    else if (((d_ani >= 19) && (m_ani == 2)) || ((d_ani <= 20) && (m_ani == 3)))
-       document.images["signo"].src = "src/assets/images/peixes.png"
-    else if (((d_ani >= 21) && (m_ani == 3)) || ((d_ani <= 19) && (m_ani == 4)))
-        document.images["signo"].src = "src/assets/images/aries.png"
+        var d_hoj = hoje.getDate(); //trouxe o dia do mês atual (do sistema)
+        var m_hoj = hoje.getMonth() + 1; //trouxe o mes atual (do sistema) correspondente de 0 até 11
+        var a_hoj = hoje.getFullYear(); //ano inteiro do sistema para cálculo da idade
 
-    else if (((d_ani >= 20) && (m_ani == 4)) || ((d_ani <= 20) && (m_ani == 5)))
-       document.images["signo"].src = "src/assets/images/touro.png"
+        if ((d_hoj == d_ani) && (m_hoj == m_ani)) {
+            document.getElementById("mensagem").style.visibility = "visible";
+            document.getElementById("mensagem").innerHTML = "P A R A B É N S...";
+        }
+        //calculo da idade 
+        var idade = a_hoj - a_ani;
 
-    else if (((d_ani >= 21) && (m_ani == 5)) || ((d_ani <= 20) && (m_ani == 6)))
-        document.images["signo"].src = "src/assets/images/gemos.png"
+        if (m_ani > m_hoj)
+            idade = idade - 1;
 
-    else if (((d_ani >= 21) && (m_ani == 6)) || ((d_ani <= 22) && (m_ani == 7)))
-        document.images["signo"].src = "src/assets/images/cancer.png"
+        else if ((m_ani == m_hoj) && (d_ani > d_hoj))
+            idade = idade - 1;
 
-    else if (((d_ani >= 23) && (m_ani == 7)) || ((d_ani <= 22) && (m_ani == 8)))
-        document.images["signo"].src = "src/assets/images/leao.png"
+        document.getElementById("mesnas").value = idade;
 
-    else if (((d_ani >= 23) && (m_ani == 8)) || ((d_ani <= 22) && (m_ani == 9)))
-        document.images["signo"].src = "src/assets/images/virgem.png"
+        /*carregando os valores do dia, mês e ano de aniversário, 
+        nas posições correspondentes do objeto Date()*/
 
-    else if (((d_ani >= 23) && (m_ani == 9)) || ((d_ani <= 22) && (m_ani == 10)))
-        document.images["signo"].src = "src/assets/images/libra.png"
+        hoje.setDate(d_ani);
+        hoje.setMonth(m_ani - 1);
+        hoje.setFullYear(a_ani);
 
-    else if (((d_ani >= 23) && (m_ani == 10)) || ((d_ani <= 21) && (m_ani == 11)))
-        document.images["signo"].src = "src/assets/images/escorpiao.png"
+        document.getElementById("txtsema").value = hoje.getDay();
+        document.getElementById("txtsema").value = dia_semana[hoje.getDay()];
+        document.getElementById("mesnas").value = nome_mes[m_ani - 1];
 
-    else if (((d_ani >= 22) && (m_ani == 11)) || ((d_ani <= 21) && (m_ani == 12)))
-        document.images["signo"].src = "src/assets/images/sagitario.png"
-    
-    else
-        document.images["signo"].src = "src/assets/images/capricornio.png"
-} 
+        if (m_ani == 1)
+            document.getElementById("mesnas").value = "Janeiro";
+        else if (m_ani == 2)
+            document.getElementById("mesnas").value = "Fevereiro";
+        else if (m_ani == 3)
+            document.getElementById("mesnas").value = "Março";
+    }
+
+    switch (m_ani) {
+        case 1:
+            {
+                document.getElementById("txtmext").value = "Janeiro";
+                break;
+            }
+
+        case 2:
+            {
+                document.getElementById("txtmext").value = "Fevereiro";
+                break;
+            }
+
+    }
+
+if (((d_ani >= 20) && (m_ani == 1)) || ((d_ani <= 18) && (m_ani == 2)))
+    document.images["signo"].src = "src/assets/images/aquario.png"
+
+else if (((d_ani >= 19) && (m_ani == 2)) || ((d_ani <= 20) && (m_ani == 3)))
+    document.images["signo"].src = "src/assets/images/peixes.png"
+else if (((d_ani >= 21) && (m_ani == 3)) || ((d_ani <= 19) && (m_ani == 4)))
+    document.images["signo"].src = "src/assets/images/aries.png"
+
+else if (((d_ani >= 20) && (m_ani == 4)) || ((d_ani <= 20) && (m_ani == 5)))
+    document.images["signo"].src = "src/assets/images/touro.png"
+
+else if (((d_ani >= 21) && (m_ani == 5)) || ((d_ani <= 20) && (m_ani == 6)))
+    document.images["signo"].src = "src/assets/images/gemos.png"
+
+else if (((d_ani >= 21) && (m_ani == 6)) || ((d_ani <= 22) && (m_ani == 7)))
+    document.images["signo"].src = "src/assets/images/cancer.png"
+
+else if (((d_ani >= 23) && (m_ani == 7)) || ((d_ani <= 22) && (m_ani == 8)))
+    document.images["signo"].src = "src/assets/images/leao.png"
+
+else if (((d_ani >= 23) && (m_ani == 8)) || ((d_ani <= 22) && (m_ani == 9)))
+    document.images["signo"].src = "src/assets/images/virgem.png"
+
+else if (((d_ani >= 23) && (m_ani == 9)) || ((d_ani <= 22) && (m_ani == 10)))
+    document.images["signo"].src = "src/assets/images/libra.png"
+
+else if (((d_ani >= 23) && (m_ani == 10)) || ((d_ani <= 21) && (m_ani == 11)))
+    document.images["signo"].src = "src/assets/images/escorpiao.png"
+
+else if (((d_ani >= 22) && (m_ani == 11)) || ((d_ani <= 21) && (m_ani == 12)))
+    document.images["signo"].src = "src/assets/images/sagitario.png"
+
+else
+    document.images["signo"].src = "src/assets/images/capricornio.png"
+}
